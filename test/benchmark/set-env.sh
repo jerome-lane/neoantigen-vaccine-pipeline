92cce3accb6d477ca483fb3bb1b7fc880b6dd31e
Jerome Lane <jerome@etcembly.io>
2023-04-19 08:35:32 +0000

    Update set env script

--- test/benchmark/set-env.sh
+++ test/benchmark/set-env.sh
@@ -1,6 +1,6 @@
 !#/bin/bash
 
 conda create -n benchmark python=3.10
-eval "$(command conda 'shell.bash' 'hook' 2> /dev/null)"
+eval "$(conda 'shell.bash' 'hook' 2> /dev/null)"
 conda activate benchmark
 pip install -r requirements.txt
